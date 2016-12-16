9d5ec8cc48809915b1fbb0a2dd3ad5de9c499102
Dan Macumber <dan.macumber@gmail.com>
2016-12-16 14:21:32 -0700

    Updating files

--- js/base.js
+++ js/base.js
@@ -1,52 +1,52 @@
-
-/* Prettyify */
-$( document ).ready(function() {
+/* Prettify */
+$(document).ready(function () {
+    $('pre code').parent().addClass('prettyprint well');
     prettyPrint();
 });
 
 
 /* Scrollspy */
-var navHeight = $('.navbar').outerHeight(true) + 10
+var navHeight = $('.navbar').outerHeight(true) + 10;
 
 $('body').scrollspy({
-    target: '.bs-sidebar',
+    target: '.bs-docs-sidebar',
     offset: navHeight
-})
+});
 
 
 /* Prevent disabled links from causing a page reload */
-$("li.disabled a").click(function() {
+$('li.disabled a').click(function () {
     event.preventDefault();
 });
 
 
 /* Adjust the scroll height of anchors to compensate for the fixed navbar */
 window.disableShift = false;
-var shiftWindow = function() {
+var shiftWindow = function () {
     if (window.disableShift) {
         window.disableShift = false;
     } else {
-        /* If we're at the bottom of the page, don't erronously scroll up */
-        var scrolledToBottomOfPage = (
-            (window.innerHeight + window.scrollY) >= document.body.offsetHeight
-        );
+        /* If we're at the bottom of the page, don't erroneously scroll up */
+        var scrolledToBottomOfPage = (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
         if (!scrolledToBottomOfPage) {
             scrollBy(0, -60);
-        };
-    };
+        }
+    }
 };
-if (location.hash) {shiftWindow();}
+if (location.hash) {
+    shiftWindow();
+}
 window.addEventListener("hashchange", shiftWindow);
 
 
 /* Deal with clicks on nav links that do not change the current anchor link. */
-$("ul.nav a" ).click(function() {
+$('ul.nav a').click(function () {
     var href = this.href;
     var suffix = location.hash;
     var matchesCurrentHash = (href.indexOf(suffix, href.length - suffix.length) !== -1);
     if (location.hash && matchesCurrentHash) {
         /* Force a single 'hashchange' event to occur after the click event */
         window.disableShift = true;
-        location.hash='';
-    };
+        location.hash = '';
+    }
 });
